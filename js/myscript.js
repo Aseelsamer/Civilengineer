94e6c0828a32a682f3d23adc2aa00b7a1b77e34c
Aseelsamer <assoul_08@hotmail.com>
2020-09-22 22:50:04 +0300

    add

--- js/myscript.js
+++ js/myscript.js
@@ -1,2 +1,28 @@
-document.write('<h2>'HELLO EVERYONE '</h2'>);
+document.write('<h2>Welcome everyone</h2>');
+
+
+var today = new Date;
+console.log(today);
+
+var name = prompt('enter your name please');
+
+var year = prompt('year of graduation');
+
+if(year>2017){
+ alert('YOU HAVE EXPERIENCE');  
+} else if (year<2019){
+    alert('YOU Are freshly graduated ');
+} else {
+    alert('zero experience');
+}
+
+
+
+
+
+
+
+
+
+
 

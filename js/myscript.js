abe2228207de3879b51b964795867c28f48e6213
Aseelsamer <assoul_08@hotmail.com>
2020-09-23 11:41:44 +0300

    edit

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,3 @@
-document.write('<h2>Welcome everyone</h2>');
 
 
 var today = new Date;
@@ -6,15 +5,56 @@ console.log(today);
 
 var name = prompt('enter your name please');
 
-var year = prompt('year of graduation');
 
-if(year>2017){
- alert('YOU HAVE EXPERIENCE');  
-} else if (year<2019){
-    alert('YOU Are freshly graduated ');
-} else {
-    alert('zero experience');
+function aseel() {
+    //input 
+    var year = prompt('year of graduation');
+    var year;
+    
+    //processing 
+    
+    if(year>2017){
+        alert('YOU HAVE EXPERIENCE');  
+    } else if (year<2019){
+        alert('YOU Are freshly graduated ');
+    } else {
+        alert('zero experience');
+    }
+    
+    //output
+    
+    document.write('<h2>Welcome everyone</h2>');
+
 }
+
+    aseel();
+    //console.log(year);
+
+    function school() {
+        //input
+        var grade = prompt('Please enter your average in percentage?');
+        var grade;
+
+        //processing
+        if(grade>70){
+            alert('GOOD!');
+       }else if (grade==80) {
+            alert('VERY GOOD!');
+       }else if (grade<90){
+           alert('EXCELLENT!');
+       }
+
+       //output
+
+       document.write('<h3>HOPE TO SEE YOU SOON');
+
+    }
+
+school();
+//console.log(grade);
+
+
+
 
 
 

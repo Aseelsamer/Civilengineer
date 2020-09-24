b6534f2abb6e7be209b3cf8c79e92ca3906f09f4
Aseelsamer <assoul_08@hotmail.com>
2020-09-24 12:11:38 +0300

    add

--- js/myscript.js
+++ js/myscript.js
@@ -57,6 +57,14 @@ function school() {
         
         }
 
+        var speed = 90;
+        
+if (speed > 80) {
+  console.log("Warning: You are driving above 80 mph");
+} else {
+  console.log("You are driving within the speed limit");
+}
+
 
     }
 
